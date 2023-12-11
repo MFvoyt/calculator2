809a99486102ed9ba757c0f175257fef26597f3e
MFvoyt <mana.fukuoka@voyt.co.jp>
2023-12-11 18:22:54 +0900

    Initial Commit

---
+++ calculator2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calculator2
+//
+//  Created by 福岡蔦子 on 2023/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calculator2/calculator2App.swift
@@ -0,0 +1,17 @@
+//
+//  calculator2App.swift
+//  calculator2
+//
+//  Created by 福岡蔦子 on 2023/12/11.
+//
+
+import SwiftUI
+
+@main
+struct calculator2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
